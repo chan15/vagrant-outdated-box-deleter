f3b9175e9833f6b0b844fd17a06a25d64b8ccadf
David <chan15tw@gmail.com>
2017-06-25 22:11:09 +0800

    Add prompt

--- clean_box.sh
+++ clean_box.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+
 if [[ -z $1 ]]; then
     echo "Need box name!"
     exit
 fi
 
 boxName=$1
-
 array=()
 array+=($(vagrant box list | grep -w $boxName | awk '{gsub(")", ""); print $3}'))
 arraySize=${#array[@]}
@@ -21,9 +21,14 @@ if [[ $arraySize == 1 ]]; then
     exit
 fi
 
-unset array[-1]
+echo -n "Start to remove outdated $boxName (y/n)?"
+read answer
 
-for (( i = 0; i < $arraySize - 1; i++ )); do
-    command=`vagrant box remove $boxName --box-version ${array[i]}`
-    echo $command
-done
+if [[ -n $answer && $answer == 'y' ]]; then
+    unset array[-1]
+
+    for (( i = 0; i < $arraySize - 1; i++ )); do
+        command=`vagrant box remove $boxName --box-version ${array[i]}`
+        echo $command
+    done
+fi
