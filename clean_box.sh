db126a0b18508e627409ed91120d7909c0ecca8b
David <chan15tw@gmail.com>
2017-06-25 21:47:42 +0800

    Init

---
+++ clean_box.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "need box name!"
+    exit
+fi
+
+boxName=$1
+
+array=()
+array+=($(vagrant box list | grep -w $boxName | awk '{gsub(")", ""); print $3}'))
+arraySize=${#array[@]}
+
+if [[ $arraySize == 0 ]]; then
+    echo "Box is not exist!"
+    exit
+fi
+
+if [[ $arraySize == 1 ]]; then
+    echo "Only one box exist!"
+    exit
+fi
+
+unset array[-1]
+
+for (( i = 0; i < $arraySize - 1; i++ )); do
+    command=`vagrant box remove $boxName --box-version ${array[i]}`
+    echo $command
+done
